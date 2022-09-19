9097d089d411d210dc071bbe9da44120ff4518a5
Nick Blackwell <nickblackwell82@gmail.com>
2022-09-19 16:40:23 -0700

    add map output type/size options reduce timeouts by using map events

--- index.js
+++ index.js
@@ -22,16 +22,18 @@ const _append = (a, b) => {
 		h: 1080,
 		out: 'map.png',
 		devtools: false,
-		exit:true
+		exit:true,
+		hideControls:true,
+		fullscreen:false,
+		headless:true,
+		mapType:'roadmap', //terrain, roadmap, hybrid, satellite
+		timeout:30
 	};
-
-	console.log(opt);
 
 	if (process.argv[2][0] == '{') {
 		opt = _append(opt, JSON.parse(process.argv[2]));
 	}
 
-	console.log(opt);
 
 	if (process.argv[2].indexOf('http') == 0) {
 		opt.url = process.argv[2];
@@ -40,53 +42,125 @@ const _append = (a, b) => {
 	console.log(opt);
 
 	if (process.argv.length > 3&&process.argv[3][0] == '{') {
-		console.log(process.argv[3]);
 		let arg3=JSON.parse(process.argv[3]);
-		console.log(arg3);
 		opt = _append(opt, arg3);
 	}
 
-	console.log(opt);
-
-
 	const browser = await puppeteer.launch({
+		headless:opt.headless,
 		devtools: opt.devtools
 	});
+
+
+	console.log(opt.url);
+
+
 	const page = await browser.newPage();
+	await page.setCacheEnabled(false);
+	await page.setViewport({
+		width: opt.w,
+		height: opt.h,
+		deviceScaleFactor: 1,
+	});
+
 
 	page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
 
-
-	//'https://rmt.geoforms.ca/proposal/13/fa2960ee93327d2623a7dba1a4396219'
 
 	await page.goto(opt.url, {
 		waitUntil: 'networkidle2'
 	});
 
+	await page.waitForTimeout(500);
 
-	await page.setViewport({
-		width: opt.w,
-		height: opt.h
-	})
-
+	await page.waitForTimeout(500);
 
 	const btn = await page.$('li.menu-project-map');
+	
 	await btn.evaluate(btn => btn.click());
 
-	await page.waitForTimeout(10000);
+	await page.waitForTimeout(1000);
 
-	await page.evaluate(() => {
-		GeoliveMapInstances[0].getFullscreenHandler().toggle();
-	});
-
-	await page.waitForTimeout(2000);
-
-	await page.screenshot({
-		'path': opt.out,
-	});
-
-	if(opt.exit){
-		await browser.close();
+	if(opt.fullscreen){
+		await page.evaluate(() => {
+			GeoliveMapInstances[0].getFullscreenHandler().toggle();
+		});
 	}
+
+
+	await page.evaluate((type) => {
+			GeoliveMapInstances[0].getBaseMap().setMapTypeId(type);
+	}, opt.mapType);
+
+
+	if(opt.hideControls){
+		await page.evaluate(() => {
+			UIMapControl.AllControls().forEach(function(c) {
+				  c.remove();
+			});
+
+			var gm=GeoliveMapInstances[0];
+			gm.getBaseMap().setOptions({
+				zoomControl:false,
+				mapTypeControl:false
+			})
+
+		});
+	}
+
+
+	
+
+
+	const mapArea = await page.$('.map-content');
+	const box = await mapArea.boundingBox(); // this method returns an array of geometric parameters of the element in pixels.
+
+
+	await page.exposeFunction('onMapReadyEvent', async () => {
+	    
+	    await page.waitForTimeout(2000);
+
+	    await page.screenshot({
+			'path': opt.out,
+			'clip':box,
+			'omitBackground':true
+		});
+
+		
+
+		console.log('Took Screenshot');
+
+		if(opt.exit){
+			await browser.close();
+		}
+	});
+
+
+	/*await*/ page.evaluate(() => {
+	    var gm=GeoliveMapInstances[0];
+
+	    gm.runOnceOnIdle(function() {
+             window.onMapReadyEvent();
+        });
+	});
+
+	console.log('Add timeout: '+opt.timeout);
+	setTimeout(async ()=>{
+
+		await page.screenshot({
+			'path': opt.out,
+			'clip':box,
+			'omitBackground':true
+		});
+
+		console.log('Took Screenshot (timeout)');
+
+		if(opt.exit){
+			await browser.close();
+		}
+
+	}, opt.timeout*1000);
+
+	
 
 })();
